f1df59079b66bed90f38aab9aaf648e39f924625
Juda <judaponce14@unitec.edu>
2020-06-12 14:33:18 -0600

    avanzando

--- Pieza.cpp
+++ Pieza.cpp
@@ -1,9 +1,42 @@
 #include "Pieza.h"
 
-Pieza::Pieza()
+Pieza::Pieza ()
 {
+
 }
 
+bool Pieza::adentro(int fi,int coll){
+	return(fi>=0 && fi <8)&& (coll>=0 && coll<8);
+}
+
+char Pieza::getChar(){
+	return this->caracter;
+}
+
+bool Pieza::getJugador(){
+	return this->jugador;
+}
+bool Pieza::movimientos(int fil, int col){
+	if(validacion(fil,col)){
+		if(Pieza* p= tablero[fil][col]){
+			delete p;
+			tablero[fil][col]=NULL;
+		}
+		tablero[fil][col]= tablero[this->fila][this->columna];
+		 tablero[this->fila][this->columna]=NULL;
+		 this->fila=fil;
+		 this->columna=col;
+		 return true;
+	}
+	return false;
+}
+
+Pieza::Pieza(int f, int c, Pieza*** t, bool j){
+	this->fila=f;
+	this->columna=c;
+	this->tablero=t;
+	this->jugador=j;
+}
 Pieza::~Pieza()
 {
 }
--- Pieza.h
+++ Pieza.h
@@ -1,12 +1,24 @@
 #ifndef PIEZA_H
 #define PIEZA_H
+#include <iostream>
 
 class Pieza
 {
 	public:
 		Pieza();
+		Pieza(int, int, Pieza***, bool);
+		char getChar();
+		bool getJugador();
+		bool movimientos(int,int);
+		bool adentro(int,int);
 		~Pieza();
 	protected:
+		Pieza*** tablero;
+		int fila,columna;
+		char caracter;
+		bool jugador;
+		virtual bool validacion(int fil, int colu)=0;
+		
 };
 
 #endif
